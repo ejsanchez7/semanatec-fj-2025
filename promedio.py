c67724526196a74c379c36eea22dcdec8c25a232
Erik Sánchez <ejsanchez@tec.mx>
2025-03-19 18:40:24 -0600

    Add list processing at promedio.py

--- promedio.py
+++ promedio.py
@@ -1,4 +1,7 @@
-def promedio(a, b, c):
-    return (a + b + c)/3
+def promedio(numeros):
+    suma = 0
+    for num in numeros:
+	suma += num
+    return suma / len(numeros)
 
-print(promedio(1, 2, 3))
+print(promedio([1, 2, 3]))
